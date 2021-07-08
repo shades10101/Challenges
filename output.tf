ae280c4cde4d1a05d621b83ddc32d5653df692d4
Shahid Ahmed <sahmed@univeris.com>
2021-07-08 18:02:16 -0400

    pls

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 ### The Ansible inventory file
 resource "local_file" "AnsibleInventory" {
-  content = templatefile("inventory.tmpl",
+  content = templatefile("/.github/workflows/inventory.tmpl",
     {
       linipv4_address = module.aws-instance.ip
     }
