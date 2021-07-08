ab325f3ab6b760fc71d7a9244751c628a5dceb6c
Shahid Ahmed <sahmed@univeris.com>
2021-07-08 10:20:11 -0400

    test

--- terraform_modules/modules/aws-instance/output.tf
+++ terraform_modules/modules/aws-instance/output.tf
@@ -2,9 +2,3 @@ output "ip" {
   description = "default ip address of the deployed VM"
   value       = aws_instance.web_server.public_ip
 }
-
-output "private_key_pem" {
-  description = "returns a string"
-  value       = tls_private_key.mykey.private_key_pem
-  sensitive   = true
-}
